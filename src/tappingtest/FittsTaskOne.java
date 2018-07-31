36a48ee115770b095c7705fe151d2ff4423254c8
Hartawan\wawan <hartawan_sugihono@yahoo.com>
2018-07-31 11:27:19 +0700

    Program

---
+++ src/tappingtest/FittsTaskOne.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tappingtest;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author wawan
+ */
+public class FittsTaskOne {
+//            JFrame frame = new JFrame();
+//JPanel panel = new JPanel();
+//frame.add(panel);
+//panel.addMouseListener(new MouseAdapter() {// provides empty implementation of all
+//                                           // MouseListener`s methods, allowing us to
+//                                           // override only those which interests us
+//    @Override //I override only one method for presentation
+//    public void mousePressed(MouseEvent e) {
+//        System.out.println(e.getX() + "," + e.getY());
+//    }
+//});
+//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//frame.setSize(200, 200);
+//frame.setVisible(true);
+//    
+}
---
+++ src/tappingtest/TappingTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tappingtest;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+
+/**
+ *
+ * @author wawan
+ */
+public class TappingTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        FrameAwal.main(args);
+//        Random rn = new Random();
+//        float width =  10 + (int)(Math.random() * 100);
+//        float distance =  300 + (int)(Math.random() * 500);
+//        float hasil = (width+distance)/width;
+//        while(hasil != 65){
+//            width =  10 + (int)(Math.random() * 100);
+//            distance =  300 + (int)(Math.random() * 500);
+//            hasil = (width+distance)/width;
+//        }
+//        System.out.println("width: "+width+"\t"+"distance: "+distance);
+    }
+    
+    
+    
+}
